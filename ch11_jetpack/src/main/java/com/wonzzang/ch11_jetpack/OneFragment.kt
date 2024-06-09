2bdcf03da9d5b92000e577a0517cebb98b446e50
Wonhee Jang <Qtsky89@gmail.com>
2024-06-09 15:35:06 +0900

    [ch11] Added fragments

--- ch11_jetpack/src/main/java/com/wonzzang/ch11_jetpack/OneFragment.kt
+++ ch11_jetpack/src/main/java/com/wonzzang/ch11_jetpack/OneFragment.kt
@@ -1,59 +1,96 @@
 package com.wonzzang.ch11_jetpack
 
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.wonzzang.ch11_jetpack.databinding.ItemRecyclerviewBinding
+import com.wonzzang.ch11_jetpack.databinding.FragmentOneBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class MyViewHolder(val binding: ItemRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root)
 
-/**
- * A simple [Fragment] subclass.
- * Use the [OneFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class OneFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+class MyAdapter(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    override fun getItemCount(): Int {
+        return datas.size;
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        MyViewHolder(ItemRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent,
+            false))
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as MyViewHolder).binding
+        binding.itemData.text = datas[position]
+    }
+
+}
+
+class MyDecoration(val context: Context): RecyclerView.ItemDecoration() {
+    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDrawOver(c, parent, state)
+
+        val width = parent.width
+        val height = parent.height
+
+        val dr: Drawable? = ResourcesCompat.getDrawable(context.resources, R.drawable.kbo, null)
+        val drWidth = dr?.intrinsicWidth
+        val drHeight = dr?.intrinsicHeight
+
+        val left = width/2 - drWidth?.div(2) as Int
+        val top = height/2 - drHeight?.div(2) as Int
+
+        c.drawBitmap(BitmapFactory.decodeResource(context.resources, R.drawable.kbo),
+                left.toFloat(), top.toFloat(), null)
+    }
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        val index = parent.getChildAdapterPosition(view)+1
+        if(index % 3 == 0)
+            outRect.set(10, 10, 10, 60)
+        else
+            outRect.set(10, 10, 10, 0)
+
+        view.setBackgroundColor(Color.parseColor("#28A0FF"))
+        ViewCompat.setElevation(view, 20.0f)
+    }
+}
+class OneFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false)
+        val binding = FragmentOneBinding.inflate(inflater, container, false)
+
+        val datas = mutableListOf<String>()
+        for(i in 1..10){
+            datas.add("Item $i")
+        }
+
+        val adapter = MyAdapter(datas)
+        val layoutManager = LinearLayoutManager(activity)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.addItemDecoration(MyDecoration(activity as Context))
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OneFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OneFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
